11f86b3db6498e5689a774982f62cfc2cb7fa3f1
marcos-silva-rodrigues <marcos.rodrigues.dev@gmail.com>
2023-01-22 12:19:00 -0300

    refactor: formatação de linha

--- src/components/SummaryTable.tsx
+++ src/components/SummaryTable.tsx
@@ -7,29 +7,29 @@ const minimumSummaryDatesSize = 18 * 7; // 18 weeks
 const amountOfDayToFill = minimumSummaryDatesSize - summaryDates.length;
 
 export function SummaryTable() {
-	return (
-		<div className="w-full flex">
-			<div className="grid grid-rows-7 grid-flow-row gap-3">
-				{weekDays.map((day, index) => (
-					<div
-						key={`${day}_${index}`}
-						className="text-zinc-400 text-xl h-10 w-10 font-bold flex items-center justify-center"
-					>
-						{day}
-					</div>
-				))}
+  return (
+    <div className="w-full flex">
+      <div className="grid grid-rows-7 grid-flow-row gap-3">
+        {weekDays.map((day, index) => (
+          <div
+            key={`${day}_${index}`}
+            className="text-zinc-400 text-xl h-10 w-10 font-bold flex items-center justify-center"
+          >
+            {day}
+          </div>
+        ))}
 
-			</div>
+      </div>
 
-			<div className="grid grid-rows-7 grid-flow-col gap-3">
-				{summaryDates.map(date => (
-					<HabitDay key={date.toString()} />
-				))}
+      <div className="grid grid-rows-7 grid-flow-col gap-3">
+        {summaryDates.map(date => (
+          <HabitDay key={date.toString()} />
+        ))}
 
-				{amountOfDayToFill > 0 && Array.from({ length: amountOfDayToFill }).map((_, index) => (
-					<div key={index} className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed" />
-				))}
-			</div>
-		</div>
-	);
+        {amountOfDayToFill > 0 && Array.from({ length: amountOfDayToFill }).map((_, index) => (
+          <div key={index} className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed" />
+        ))}
+      </div>
+    </div>
+  );
 }
