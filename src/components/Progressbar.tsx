2374ff674fec2d70719cb394a5615f8fdac8ad5d
marcos-silva-rodrigues <marcos.rodrigues.dev@gmail.com>
2023-01-22 12:52:20 -0300

    feat: barra de progresso

---
+++ src/components/Progressbar.tsx
@@ -0,0 +1,20 @@
+type ProgressBarProps = {
+  progress: number;
+}
+
+export function ProgressBar(props: ProgressBarProps) {
+  const progressStyles = {
+    width: `${props.progress}%`
+  }
+  return (
+    <div className="h-3 rounded-xl bg-zinc-700 w-full mt-4">
+      <div
+        role="progressbar"
+        aria-label="progresso de hábitos completados nesse dia"
+        aria-value={props.progress}
+        className="h-3 rounded-xl bg-violet-600"
+        style={progressStyles}
+      />
+    </div>
+  );
+}
